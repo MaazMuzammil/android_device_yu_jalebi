eeb2ef8b3cdc96cbab0293712bf7ef2e58b5806d
dev-harsh1998 <harshit.india@linaro.org>
2018-11-13 11:44:28 +0000

    jalebi: Switch to kernel thermal management [1/1] * MSM simple thermal driver

--- device.mk
+++ device.mk
@@ -117,10 +117,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # USB ID
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.usb.id.midi=90BA \
